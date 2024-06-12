ea7d6c7b989e2555e78aa1145ab754b27d3275da
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-06-12 22:19:58 +0300

    Made necesessary imports

---
+++ models/task.py
@@ -0,0 +1,5 @@
+
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from database.connection import Base
+from datetime import datetime, timedelta

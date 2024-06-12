fed3a920c2df376b4b8b7892018b6223613c7746
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-06-12 22:13:08 +0300

    Corrected code

--- models/task_label.py
+++ models/task_label.py
@@ -1,2 +1,8 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from database.connection import Base
+
+class TaskLabel(Base):
+    __tablename__ = "task_label"
+
+    task_id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    label_id = Column(Integer, ForeignKey("labels.id"), primary_key=True)

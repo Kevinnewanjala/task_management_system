b9091ba62c988b339723a19de51c5b1fd62eee1a
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-06-12 22:15:16 +0300

    Complete code

--- models/task_name.py
+++ models/task_name.py
@@ -1,2 +1,9 @@
+
 from sqlalchemy import Column, Integer, ForeignKey
 from database.connection import Base
+
+class TaskName(Base):
+    __tablename__ = "task_name"
+
+    task_id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    name_id = Column(Integer, ForeignKey("names.id"), primary_key=True)

630df62ce0ac366f4e58bb423c55d422668cf76e
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-06-12 22:06:45 +0300

    Added imports to main file

---
+++ app.py
@@ -0,0 +1,5 @@
+from models.name import create_name, delete_name, update_name, read_names
+from models.task import create_task, delete_task, update_task, read_tasks
+from models.label import create_label, delete_label, update_label, read_labels
+from database.connection import create_db_session
+from datetime import datetime
--- models/label.py
+++ models/label.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database.connection import Base
